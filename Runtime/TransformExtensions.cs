ee556e5cf26f317d34fb12f80a8b530c3320ebc6
Adrian Yip <adrian@visionaries777.com>
2025-05-12 15:52:46 +0800

    Get the world transform from another parent without changing the actual parent.

--- Runtime/TransformExtensions.cs
+++ Runtime/TransformExtensions.cs
@@ -23,5 +23,29 @@ namespace vz777.Foundations
             
             return builder.ToString().TrimStart('\\');
         }
+
+        /// <summary>
+        /// Get the world transform of the other parent.
+        /// </summary>
+        public static (Vector3 position, Quaternion rotation, Vector3 scale) GetWorldTransformFromOtherParent(
+            this Transform transform, Transform otherParent)
+        {
+            var scaledPosition = new Vector3(
+                transform.localPosition.x * otherParent.lossyScale.x,
+                transform.localPosition.y * otherParent.lossyScale.y,
+                transform.localPosition.z * otherParent.lossyScale.z
+            );
+        
+            var position = otherParent.position + otherParent.rotation * scaledPosition;
+            var rotation = otherParent.rotation * transform.localRotation;
+            var parentScale = transform.parent ? transform.parent.lossyScale : Vector3.one;
+            var scale = new Vector3(
+                otherParent.lossyScale.x / parentScale.x * transform.localScale.x,
+                otherParent.lossyScale.y / parentScale.y * transform.localScale.y,
+                otherParent.lossyScale.z / parentScale.z * transform.localScale.z
+            );
+
+            return (position, rotation, scale);
+        } 
     }
 }
